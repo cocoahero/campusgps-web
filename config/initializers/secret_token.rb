a51824382b77e04e6d393ca143a996e90794352d
Jonathan Baker <jbaker@cocoahero.com>
2010-10-23 22:08:32 -0400

    Adding initial rails project to repo.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CampusGPSWeb::Application.config.secret_token = '3b2b996c71a568cbdda2a6ff56ecda9b3eb7038c2b0fc50b6e6481301beea876429ef2510c032602c117d1e83895032cd5d80034845f6aca7337b6bab8f2db30'
