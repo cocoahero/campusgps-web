2618810c3c8362e2255e39bcf5e9cad51503445f
Jonathan Baker <jbaker@cocoahero.com>
2010-10-25 17:13:06 -0400

    Added page title helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def page_title
+    default_title = "CampusGPS"
+    if @page_name.nil?
+      default_title
+    else
+      "#{@page_name} | #{default_title}"
+    end
+  end
 end
