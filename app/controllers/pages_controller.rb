96dea736c517e5100c4931ac8992085b23ca5539
Jonathan Baker <jonathanmbaker@me.com>
2011-02-14 10:24:58 -0500

    Moving along...

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,21 @@
 class PagesController < ApplicationController
   def welcome
-    @page_name = 'Welcome'
+    @page_title = 'Welcome'
+  end
+  
+  def privacy
+    @page_title = 'Privacy Policy'
+  end
+  
+  def tos
+    @page_title = 'Terms of Use'
+  end
+  
+  def support
+    @page_title = 'Support'
+  end
+  
+  def contact
+    @page_title = 'Contact'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ CampusGPSWeb::Application.routes.draw do
    
   root :to => 'pages#welcome'
   
+  match "tos" => "pages#tos"
+  match "privacy" => "pages#privacy"
+  match "support" => "pages#support"
+  match "contact" => "pages#contact"
+  
 end
